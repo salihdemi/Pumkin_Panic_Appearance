cd0bb1813913209bdf55326fc583aa3cfc5931ba
salihdemi <81927519+salihdemi@users.noreply.github.com>
2026-07-12 16:26:37 +0300

    Animasyon fln

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PumkinPlayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float walkSpeed = 5f;
     public Animator anim;
